fc7b1c8569923f69d9860f4d432a4d4234e18d60
iammahdi <mahdializada761@gmial.com>
2022-03-07 13:41:31 +0430

    new changes

--- database/migrations/2022_03_07_085643_create_salarybases_table.php
+++ database/migrations/2022_03_07_085643_create_salarybases_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('salarybases', function (Blueprint $table) {
             $table->id();
+            $table->string('base_titile');
+            $table->enum('base_type',['monthly','weekly','daily','hourly']);
+            $table->int('basic_salary');
+            $table->int('currency');
+            $table->int('hourly_overtime_cost');
             
             $table->timestamps();
         });
