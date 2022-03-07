052531337e6b7366bcf0789140e9ab2d16840fc0
Mortaza10 <mortazaabbasi10@gmail.com>
2022-03-07 15:32:23 +0430

    commit

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -16,24 +16,7 @@ class employee extends Model
         return $this->hasMany('App\Model\payment');
 
     }
-<<<<<<< HEAD
     public function employee_specific_deductions(){
         return $this->hasMany('App\Models\employee_specific_deduction');
-=======
-
-    public function salarybases()
-    {
-        return $this->belongsTo('App\Models\salarybase');
-    }
-    
-    public function bonuses()
-    {
-        return $this->hasMany(Bonuses::class);
-    }
-
-    public function overtimes()
-    {
-        return $this->hasMany(Overtimes::class);
->>>>>>> 4044ba73a68de9bb795cde5d55200d3f03c9be19
     }
 }
