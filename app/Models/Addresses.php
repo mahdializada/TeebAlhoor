6de403415d0b400642e5953777484c1f3ce3f951
Mortaza10 <mortazaabbasi10@gmail.com>
2022-03-07 16:38:35 +0430

    commit

--- app/Models/Addresses.php
+++ app/Models/Addresses.php
@@ -8,29 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Addresses extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-
-    protected $table="addresses";
-
-   public function countries()
-   {
-    return $this->belongsTo('App\Models\Country');
-   }
-=======
    
     protected $table="addresses";
-<<<<<<<< HEAD:app/Models/Addresses.php
+
     public function state()
     {
         return $this->belongsTo('App\Models\state');
     }
         
-========
+
 
     public function countries()
     {
         return $this->belongsTo('App\Models\Country');
     }       
->>>>>>>> 8d7f40b286e2d3a168b6936ab4335eddb9de2d36:app/Models/Addresse.php
->>>>>>> 8d7f40b286e2d3a168b6936ab4335eddb9de2d36
+p
+
 }
