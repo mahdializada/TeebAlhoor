2677470c1dd1cede94b7e5662ef2c121d43e0742
hashmat481 <hashmatullahh481@gmail.com>
2022-03-07 01:04:08 -0800

    migration
    migrate

---
+++ database/migrations/2022_03_07_084944_create_expences_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expences', function (Blueprint $table) {
+            $table->id();
+            $table->string('target_name');
+            $table->bigInteger('traget_qty');
+            $table->bigInteger('unit_price');
+            $table->string('unit');
+            $table->string('currency');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expences');
+    }
+};
