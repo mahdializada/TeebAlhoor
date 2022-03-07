f5316adf1283814a671733926204002a32305aee
hashmat481 <hashmatullahh481@gmail.com>
2022-03-07 03:56:43 -0800

    address
    1

---
+++ app/Models/Addresses.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Addresses extends Model
+{
+    use HasFactory;
+
+    protected $table="addresses";
+
+   public function countries()
+   {
+    return $this->belongsTo('App\Models\Country');
+   }
+}
