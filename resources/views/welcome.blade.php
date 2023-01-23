ddb114c1685ee76a19a3434475845ee94d94d2fa
mahdializada <mahdializada761@gmail.com>
2023-01-23 09:58:08 +0430

    fdfdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,6 +79,7 @@
 
                                     offers thousands o f video
 
+
                                     tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                                 </div>
                             </div>
