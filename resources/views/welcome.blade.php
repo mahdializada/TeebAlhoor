dad1cc281a998bff03878b5cc63c909ad913ea63
mahdializada <mahdializada761@gmail.com>
2023-01-24 12:57:24 +0430

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@ s
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Lar  avel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
