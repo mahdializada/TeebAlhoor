e2184bdb622f97495dc96db5098eaa2ebc48e1b1
mahdializada <mahdializada761@gmail.com>
2023-01-24 11:01:15 +0430

    ddd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 
-                                    offers thousands o f videodf
+                                    offers thousands o
 
 
                                     tutorials on cvLarasss  vel,dd PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
