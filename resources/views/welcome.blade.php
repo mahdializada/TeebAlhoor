50bf1d0c2b80b64ad6c7c4330fa64632efc681e4
mahdializada <mahdializada761@gmail.com>
2023-01-24 10:48:15 +0430

    dfd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has sfslfjl won   ,fdfjl derful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Laravel hasdd sfslfjl won   ,fdfjl derful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
 
 
