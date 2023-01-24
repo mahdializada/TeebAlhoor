fac35cb1376306a80e6e8d7681816fea729c2122
mahdializada <mahdializada761@gmail.com>
2023-01-24 10:48:30 +0430

    dffd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laracasts
+                                    Laracastskdf
     dssd
                                     offers thousands o f videodf
 
