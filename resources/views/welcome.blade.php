8454fac838d4037d12b2e3ea4180471f93ac29fd
mahdializada <mahdializada761@gmail.com>
2023-01-24 12:57:34 +0430

    ddd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@ s
 
                                     offers th
 
-                                    tutorials on cvLarass  sll  vel,dd PHP, and JavaScript development.  Check them out, see for yourself, and massively level up your development skills in the process.
+                                    tutor  ials on cvLarass  sll  vel,dd PHP, and JavaScript development.  Check them out, see for yourself, and massively level up your development skills in the process.
                                 </div>
                             </div>
                         </div>
