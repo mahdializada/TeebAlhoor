73ccd47242744985726bff4a750a3c98c8f09b79
mahdializada <mahdializada761@gmail.com>
2023-01-24 13:31:09 +0430

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@ s
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 
-                                    offers the dj
+                                    offers the d
 
                                     tutorals on cvLarel,dd PHP, and JavaScript development.  Check them out, see for yourself, and massively level up your development skills in the process.
                                 </div>
